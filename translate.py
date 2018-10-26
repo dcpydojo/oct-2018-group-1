27a12f38258db50e09ad09bd61010953c8b49347
Brian Austin <austin.brian@gmail.com>
2018-10-25 20:38:27 -0400

    small additional changes -- still needs api call

--- translate.py
+++ translate.py
@@ -1,17 +1,22 @@
 import json
 import sys
 
+
 def translated_phrase(phrase, lang):
     t = hit_the_api(phrase, lang)
     return t
 
-def translate(phrase,
-              from='en',
-              to_languages=['spanish','chinese','russian']):
+
+def translate(phrase, origin_lang='en',
+              to_languages=['spanish', 'chinese', 'russian']):
     for lang in to_languages:
         phrase = translated_phrase(phrase, lang)
     return phrase
 
+
 if __name__ == '__main__':
-    translation = translate('success')
-    print(translation)
+    phrase = sys.argv[1]
+    language = sys.argv[2]
+    # translation = translate('success')
+    print(sys.argv[0])
+    print(f"{phrase} --{language}")
